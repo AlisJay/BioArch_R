4cdf8bf75fc657489ee1e2dbcf0e4405dbcfa2e6
AlisJay <alisonjaynenicholson@live.co.uk>
2017-03-31 13:26:24 +0100

    Moved Write tab in BAInventory

--- Apps/BAInventory/ui.R
+++ Apps/BAInventory/ui.R
@@ -1,29 +1,6 @@
 library(shiny);library(markdown)
 
 shinyUI(navbarPage("Inventory",
-                   tabPanel("write",
-                            textInput("dir","Data Folder",value="../../data/"),
-                            column(width=4,
-                                   h3("Population Info"),
-                                   textInput("POPID","Population ID",value="NA"),
-                                   textInput("POPName","Population name",value="NA"),
-                                   textInput("Person1","Investigator",value="NA"),
-                                   actionButton("Create","Create new file"),
-                                   h4(textOutput("PMessage"))
-                                   ),
-                            column(width=4,
-                                   h3("Individual Info"),
-                                   textInput("ID","Individual ID",value="NA"),
-                                   textInput("Person2","Investigator intials",value="NA"),
-                                   actionButton("Append","Add individual"),
-                                   h4(textOutput("IMessage"))
-                                   ),
-                            column(width=4,
-                                   h3("Compatability check"),
-                                   actionButton("Compat","Check file"),
-                                   h4(textOutput("ComMessage"))
-                                   )
-                            ),
                    tabPanel("Skull",
                             fluidPage(
                               column(width=4,
@@ -385,6 +362,29 @@ shinyUI(navbarPage("Inventory",
                                    h4("Features"),
                                    h5(textOutput("CustomFeatures"))
                             )
+                            )
+                   ),
+                   tabPanel("write",
+                            textInput("dir","Data Folder",value="../../data/"),
+                            column(width=4,
+                                   h3("Population Info"),
+                                   textInput("POPID","Population ID",value="NA"),
+                                   textInput("POPName","Population name",value="NA"),
+                                   textInput("Person1","Investigator",value="NA"),
+                                   actionButton("Create","Create new file"),
+                                   h4(textOutput("PMessage"))
+                            ),
+                            column(width=4,
+                                   h3("Individual Info"),
+                                   textInput("ID","Individual ID",value="NA"),
+                                   textInput("Person2","Investigator intials",value="NA"),
+                                   actionButton("Append","Add individual"),
+                                   h4(textOutput("IMessage"))
+                            ),
+                            column(width=4,
+                                   h3("Compatability check"),
+                                   actionButton("Compat","Check file"),
+                                   h4(textOutput("ComMessage"))
                             )
                    )
 )
